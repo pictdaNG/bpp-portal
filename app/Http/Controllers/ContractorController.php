874eb1b2aef83602f3777f13994591076f0fd2ae
retnan <dretnan@logicaladdress.com>
2019-04-06 10:32:13 +0000

    Last commit

--- app/Http/Controllers/ContractorController.php
+++ app/Http/Controllers/ContractorController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class ContractorController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
     public function registration(Request $request){
         return view('contractor/registration');
     }
