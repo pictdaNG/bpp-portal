cdf021f6b574affb5a0f719034ec6bbb2f8a69a0
vixxjy <ajorvictor48@gmail.com>
2019-04-12 10:20:33 +0100

    seeder country updated

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -13,168 +13,168 @@ class StatesTableSeeder extends Seeder
     {
         DB::table('states')->insert([[
             'name' => 'Abia',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Adamawa',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Akwa Ibom',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Anambra',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Bauchi',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Bayelsa',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Benue',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Borno',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Cross River',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Delta',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Ebonyi',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Edo',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
 
         DB::table('states')->insert([[
             'name' => 'Ekiti',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Enugu',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Gombe',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Imo',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Jigawa',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Kaduna',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Kano',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Katsina',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Kebbi',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Kogi',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Kwara',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Lagos',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
 
         DB::table('states')->insert([[
             'name' => 'Nasarawa',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Niger',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Ogun',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Ondo',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Osun',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Oyo',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Plateau',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Rivers',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Sokoto',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Taraba',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'Yobe',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
         DB::table('states')->insert([[
             'name' => 'Zamfara',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
 
         DB::table('states')->insert([[
             'name' => 'FCT',
-            'country_id' => '1'
+            // 'country_id' => '1'
         ]]);
     }
 }
