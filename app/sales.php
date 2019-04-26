b12833005ebc3c2bdbebe0fdf99832b293dea84d
eddie <edwardobande36@gmail.com>
2019-04-26 17:58:16 +0100

    added sales model

--- app/sales.php
+++ app/sales.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class sales extends Model
+class Sales extends Model
 {
     protected $fillable = ['advert_lot_id', 'lot_description', 'advert_id', 'advert_introduction', 'advert_name',  'amount', 'user_id', 'user_name', 'mda_id', 'mda_name'];
 
